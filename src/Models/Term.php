25e7b63e41d775595c17648262022f7c0a4fe2fe
Papp Krisztian <fejlesztes@letscode.hu>
2016-07-17 01:38:27 +0200

    Term related models added.

---
+++ src/Models/Term.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Letscodehu\Larablog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Term extends Model
+{
+    protected $table = "wp_terms";
+    public $timestamps = false;
+    protected $fillable = ["name", "slug"];
+
+    public function taxonomies() {
+        return $this->hasOne('\App\Models\TermTaxonomy', "term_id", "term_id");
+    }
+
+    public function category() {
+        return $this->taxonomies()->where("taxonomy", "category")->first();
+    }
+
+    public function tag() {
+        return $this->taxonomies()->where("taxonomy", "post_tag")->first();
+    }
+
+}
---
+++ src/Models/TermRelationship.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Letscodehu\Larablog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TermRelationship extends Model
+{
+    protected $table = "wp_term_relationships";
+
+    public function taxonomies() {
+        return $this->hasMany('\App\Models\TermTaxonomy', "term_taxonomy_id", "term_taxonomy_id");
+    }
+
+    public function term() {
+
+    }
+
+
+    public $timestamps = false;
+    protected $fillable = [
+        "object_id",
+        "term_taxonomy_id",
+        "term_order"
+    ];
+}
