53b42c9e15ae0a6ea6d772ed1493c48cc6dff620
Papp Krisztian <fejlesztes@letscode.hu>
2016-07-17 01:37:47 +0200

    TermTaxonomy class added.

---
+++ src/Models/TermTaxonomy.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Letscodehu\Larablog\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TermTaxonomy extends Model
+{
+    protected $table = "wp_term_taxonomy";
+
+    protected $primaryKey = "term_taxonomy_id";
+
+    public $timestamps = false;
+    protected $fillable = ["term_id", "taxonomy", "parent", "count"];
+
+    public function term() {
+        return $this->hasOne('\App\Models\Term', "term_id");
+    }
+
+    public function getName() {
+        return $this->term->name;
+    }
+
+    public function getSlug() {
+        return $this->term->slug;
+    }
+
+    public function related() {
+        return $this->belongsToMany('\App\Models\Post', "wp_term_relationships", "term_taxonomy_id", "object_id")
+            ->where("post_status", "publish")
+            ->where("post_type", "post");
+    }
+
+    public function getCount() {
+        return $this->count;
+    }
+    public function getFontSize() {
+        return $this->fontsize;
+    }
+}
